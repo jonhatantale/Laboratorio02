134959b2200daa0fcafce06d02732e0095dc9460
CarlosToledo110 <carlostoledo1250@gmail.com>
2026-08-17 12:41:13 -0600

    feat(calc): agrega calculadora basica

---
+++ Calculadora.cs
@@ -0,0 +1,11 @@
+﻿public static class Calculadora
+{
+    public static int sumar(int a, int b)
+    {
+        return a - b;
+    }
+    public static int restar(int a, int b)
+    {
+        return a - b;
+    }
+}
